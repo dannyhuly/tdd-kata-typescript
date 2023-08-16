43a4e34de0fc3ff4035d1b3b4d2d140305d78262
Daniel Huly <dh436v@intl.att.com>
2023-08-16 09:39:38 +0300

    stage 1 - test preparation

---
+++ src/StringCalculator/StringCalculator.spec.ts
@@ -0,0 +1,7 @@
+describe('StringCalculator',() => {
+    describe('add', () => {
+        it.todo('should return 0 on empty string')
+        it.todo('should return the value of a single number')
+        it.todo('should return the sum of two numbers separated by commas')
+    })
+})
