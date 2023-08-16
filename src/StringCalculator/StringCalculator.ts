b0cfb9698dbe0781c91a962fea2e60e3751231c7
Daniel Huly <dh436v@intl.att.com>
2023-08-16 09:56:59 +0300

    stage 1 - solution

--- src/StringCalculator/StringCalculator.ts
+++ src/StringCalculator/StringCalculator.ts
@@ -1,3 +1,13 @@
-export function add(number: string) : number {
-    throw new Error('Not Implemented');
+export function add(number: string): number {
+    if (number == "") {
+        return 0;
+    }
+
+    const [num1, num2] = number.split(',');
+    let total = parseInt(num1);
+
+    if (num2) {
+        total += parseInt(num2);
+    }
+    return total;
 }
