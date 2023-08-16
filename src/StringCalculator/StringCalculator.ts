5beab122a96e03471d5f20f03c9f17e1a879c4bd
Daniel Huly <dh436v@intl.att.com>
2023-08-16 17:03:21 +0300

    stage 4 - solution

--- src/StringCalculator/StringCalculator.ts
+++ src/StringCalculator/StringCalculator.ts
@@ -3,9 +3,18 @@ export function add(number: string): number {
         return 0;
     }
 
-    const total = number
-        .replace('\n', ',')
-        .split(',')
+    let numberString = number;
+    let delimiter = ',';
+    
+    if (number.includes('//')) {
+        const [delimiterFormatted, numberStr] = number.split(/\n(.*)/s, 2); // split string by "\n" first acutance 
+        delimiter = delimiterFormatted.substring(2);
+        numberString = numberStr;
+    }
+
+    const total = numberString
+        .replace('\n', delimiter)
+        .split(delimiter)
         .map((v) => parseInt(v))
         .reduce((acu, number) => acu + number, 0)
 
